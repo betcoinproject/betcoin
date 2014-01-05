2a47669e4c8ce36abdc811bc26c3a239201f276d
root <root@d1stkfactory>
2014-01-05 12:53:48 +0000

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("BET");
+    case mBTC: return QString("mBET");
+    case uBTC: return QString::fromUtf8("μBET");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("BetCoin");
+    case mBTC: return QString("Milli-BetCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-BetCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
